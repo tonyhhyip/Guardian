a146feeb2c3ecdcf995c4fe1c672841c7233b469
Tony Yip <tony@opensource.hk>
2016-08-13 00:24:43 +0800

    Add Helper Function

--- lib/Controllers/Controller.php
+++ lib/Controllers/Controller.php
@@ -3,6 +3,7 @@
 namespace Guardian\Controllers;
 
 use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Log\Writer;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -10,4 +11,31 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected $logger;
+
+    public function __construct(Writer $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    protected function getSuccessResponse()
+    {
+        return response()->json(['result' => 'success']);
+    }
+
+    protected function getInvalidResponse()
+    {
+        return response()->json(['result' => 'failed'], 422);
+    }
+
+    protected function getCreatedResponse()
+    {
+        return response()->json(['result' => 'success'], 201);
+    }
+
+    protected function getFailedResponse()
+    {
+        return response()->json(['result' => 'failed'], 500);
+    }
 }
